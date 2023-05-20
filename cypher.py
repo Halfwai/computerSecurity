3517a1d15be92f6c12cf524625a49da72bd0c232
Halfwai <wai_sanlee@hotmail.com>
2023-05-20 22:53:10 +0800

    replaces seperate functions

--- cypher.py
+++ cypher.py
@@ -1,5 +1,35 @@
 import math
 
+def vigenere(key, phrase):
+    offset = ord("a")
+    limit = ord("z")
+    encryptedPhrase = []
+    for i in range(0, len(phrase)):
+        shift = ord(key[i % len(key)]) - offset
+        newChar = ord(phrase[i]) + shift
+        if newChar > limit:
+            newChar -= 26
+        encryptedPhrase.append(chr(newChar))
+    return ''.join(encryptedPhrase)
+
+def transposition(key, phrase):
+    grid = []
+    for i in range(len(key)):
+        index = i
+        row = []
+        while index <= len(phrase) - 1:
+            row.append(phrase[index])
+            index += len(key)
+        grid.append(row)
+    alphabet = "abcdefghijklmnopqrstuvwqyz"
+    encryptedPhrase = []
+    for i in range(len(alphabet)):
+        for j in range(len(key)):
+            if alphabet[i] == key[j]:
+                for letter in grid[j]:
+                    encryptedPhrase.append(letter)
+    return "".join(encryptedPhrase)
+
 def encrypt(firstKey, secondKey, phrase):
     offset = ord("a")
     limit = ord("z")
@@ -26,34 +56,6 @@ def encrypt(firstKey, secondKey, phrase):
                 for letter in grid[j]:
                     encryptedPhrase.append(letter)
     return "".join(encryptedPhrase)
-
-def deCrypt(firstKey, secondKey, phrase):
-    alphabet = "abcdefghijklmnopqrstuvwqyz"
-    grid = []
-    for i in range(len(secondKey)):
-        grid.append([])
-    phraseIndex = 0
-    for i in range(len(alphabet)):
-        for j in range(len(secondKey)):
-            if alphabet[i] == secondKey[j]:
-                inputNumber = math.floor(len(phrase)/len(secondKey))
-                if len(phrase) % len(secondKey) > j:
-                    inputNumber += 1
-                for k in range(phraseIndex, phraseIndex + inputNumber):
-                    grid[j].append(phrase[k])
-                phraseIndex += inputNumber
-    partlyUnencryptedPhrase = []
-    for i in range(len(phrase)):
-        partlyUnencryptedPhrase.append(grid[i % len(secondKey)][math.floor(i/len(secondKey))])
-    offset = ord("a")
-    unencryptedPhrase = []
-    for i in range(0, len(partlyUnencryptedPhrase)):
-        shift = ord(firstKey[i % len(firstKey)]) - offset
-        newChar = ord(partlyUnencryptedPhrase[i]) - shift
-        if newChar < offset:
-            newChar += 26
-        unencryptedPhrase.append(chr(newChar))
-    return ''.join(unencryptedPhrase)
 
 def deTransposition(key, phrase):
     alphabet = "abcdefghijklmnopqrstuvwqyz"
@@ -86,6 +88,34 @@ def deVigenere(key, phrase):
         unencryptedPhrase.append(chr(newChar))
     return ''.join(unencryptedPhrase)
 
+def deCrypt(firstKey, secondKey, phrase):
+    alphabet = "abcdefghijklmnopqrstuvwqyz"
+    grid = []
+    for i in range(len(secondKey)):
+        grid.append([])
+    phraseIndex = 0
+    for i in range(len(alphabet)):
+        for j in range(len(secondKey)):
+            if alphabet[i] == secondKey[j]:
+                inputNumber = math.floor(len(phrase)/len(secondKey))
+                if len(phrase) % len(secondKey) > j:
+                    inputNumber += 1
+                for k in range(phraseIndex, phraseIndex + inputNumber):
+                    grid[j].append(phrase[k])
+                phraseIndex += inputNumber
+    partlyUnencryptedPhrase = []
+    for i in range(len(phrase)):
+        partlyUnencryptedPhrase.append(grid[i % len(secondKey)][math.floor(i/len(secondKey))])
+    offset = ord("a")
+    unencryptedPhrase = []
+    for i in range(0, len(partlyUnencryptedPhrase)):
+        shift = ord(firstKey[i % len(firstKey)]) - offset
+        newChar = ord(partlyUnencryptedPhrase[i]) - shift
+        if newChar < offset:
+            newChar += 26
+        unencryptedPhrase.append(chr(newChar))
+    return ''.join(unencryptedPhrase)
+
 
 phrase = "countless path one destination"
 
@@ -97,12 +127,22 @@ phrase = phrase.replace(" ", "")
 
 print(phrase)
 
-encryptedPhrase = (encrypt(vigKey, transKey, phrase))
+encryptedPhrase = (vigenere(vigKey, phrase))
+
+# print(encryptedPhrase)
+
+encryptedPhrase = transposition(transKey, encryptedPhrase)
 
 print(encryptedPhrase)
 
-deCryptedPhrase = deCrypt(vigKey, transKey, encryptedPhrase)
+print(encrypt(vigKey, transKey, phrase))
 
-print(deCryptedPhrase)
+solvedPhrase = deTransposition(transKey, encryptedPhrase)
+
+solvedPhrase = deVigenere(vigKey, solvedPhrase)
+
+print(solvedPhrase)
+
+print(deCrypt(vigKey, transKey, encryptedPhrase))
 
 
